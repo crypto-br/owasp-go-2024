61fa75667c1a460e4b8caff4cc753e94406ae96f
Luiz Machado <luizguilhermemmj@gmail.com>
2024-08-10 01:45:18 -0300

    Corrigido o uso de SecureRandom no código Java

--- exemplo/java/example1/src/main/java/com/mycompany/app/App.java
+++ exemplo/java/example1/src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,10 @@
-package com.mycompany.app;
-import java.util.Random;
+import java.security.SecureRandom;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Random rand = new Random();
-        System.out.println(rand.nextInt(50));
-        System.out.println( "Hello World!" );
+    public class App {
+        public static void main(String[] args) {
+            SecureRandom secureRandom = new SecureRandom();
+            int randomNumber = secureRandom.nextInt(100); // Gera um número aleatório de 0 a 99 de forma segura
+            System.out.println(randomNumber);
+        }
     }
-}
+    
