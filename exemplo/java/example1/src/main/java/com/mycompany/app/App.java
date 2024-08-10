9e02c30d46616705cde24ccb8b08b6ec6ca66dfa
Luiz Machado <luizguilhermemmj@gmail.com>
2024-08-10 14:13:30 -0300

    Sugestão de correção de código

--- exemplo/java/example1/src/main/java/com/mycompany/app/App.java
+++ exemplo/java/example1/src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,6 @@
-package com.mycompany.app;
-import java.util.Random;
+```java
+import java.security.SecureRandom;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Random rand = new Random();
-        System.out.println(rand.nextInt(50));
-        System.out.println( "Hello World!" );
-    }
-}
+SecureRandom secureRandom = new SecureRandom();
+int randomNumber = secureRandom.nextInt(100); // Gera um número aleatório entre 0 e 99
+```
